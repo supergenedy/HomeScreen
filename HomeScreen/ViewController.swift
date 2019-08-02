c8f42c232c6e952bf9df7b8c1736bc6b94d31cee
supergenedy <supergenedy@gmaill.com>
2019-08-03 00:19:59 +0200

    Initial Commit

---
+++ HomeScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeScreen
+//
+//  Created by Ahmed on 8/3/19.
+//  Copyright © 2019 supergenedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
